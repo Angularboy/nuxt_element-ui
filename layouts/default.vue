7e9c5bd7610e5e7b733d992d46d222c5320af2f8
liuyiqiao <liuyiqiao@ftxmall.com>
2019-03-04 10:29:32 +0800

    q

---
+++ layouts/default.vue
@@ -0,0 +1,322 @@
+<template>
+  <div>
+    <el-container>
+      <el-header style="background: #336699;">
+        1
+      </el-header>
+    </el-container>
+    <el-container style="height: 400px;">
+      <el-aside><nuxt/></el-aside>
+      <el-main>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br>地方 <br></el-main>
+    </el-container>
+  </div>
+</template>
+
+<style>
+  html {
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    font-size: 62.5%;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    font-size: 1.4rem;
+    line-height: 1.5;
+    color: #333333;
+    background-color: #f7f7f7;
+    height: 100%;
+    overflow-x: hidden;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  article, aside, details, figcaption, figure, footer, header, hgroup, main, nav, section, summary {
+    display: block;
+  }
+
+  audio, canvas, progress, video {
+    display: inline-block;
+    vertical-align: baseline;
+  }
+
+  audio:not([controls]) {
+    display: none;
+    height: 0;
+  }
+
+  [hidden], template {
+    display: none;
+  }
+
+  svg:not(:root) {
+    overflow: hidden;
+  }
+
+  a {
+    background: transparent;
+    text-decoration: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    color: #0088cc;
+  }
+
+  a:active {
+    outline: 0;
+  }
+
+  a:active {
+    /*color: #006699;*/
+  }
+
+  abbr[title] {
+    border-bottom: 1px dotted;
+  }
+
+  b, strong {
+    font-weight: bold;
+  }
+
+  dfn {
+    font-style: italic;
+  }
+
+  mark {
+    background: #ff0;
+    color: #000;
+  }
+
+  small {
+    font-size: 80%;
+  }
+
+  sub, sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+  }
+
+  sup {
+    top: -0.5em;
+  }
+
+  sub {
+    bottom: -0.25em;
+  }
+
+  img {
+    border: 0;
+    vertical-align: middle;
+  }
+
+  hr {
+    box-sizing: content-box;
+    height: 0;
+  }
+
+  pre {
+    overflow: auto;
+    white-space: pre;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+
+  code, kbd, pre, samp {
+    font-family: monospace, monospace;
+    font-size: 1em;
+  }
+
+  button, input, optgroup, select, textarea {
+    color: inherit;
+    font: inherit;
+    margin: 0;
+  }
+
+  button {
+    overflow: visible;
+  }
+
+  button, select {
+    text-transform: none;
+  }
+
+  button, html input[type="button"], input[type="reset"], input[type="submit"] {
+    -webkit-appearance: button;
+    cursor: pointer;
+  }
+
+  button[disabled], html input[disabled] {
+    cursor: default;
+  }
+
+  button::-moz-focus-inner, input::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+
+  input {
+    line-height: normal;
+  }
+
+  input[type="checkbox"], input[type="radio"] {
+    box-sizing: border-box;
+    padding: 0;
+  }
+
+  input[type="number"]::-webkit-inner-spin-button, input[type="number"]::-webkit-outer-spin-button {
+    height: auto;
+  }
+
+  input[type="search"] {
+    -webkit-appearance: textfield;
+    box-sizing: border-box;
+  }
+
+  input[type="search"]::-webkit-search-cancel-button, input[type="search"]::-webkit-search-decoration {
+    -webkit-appearance: none;
+  }
+
+  fieldset {
+    border: 1px solid #c0c0c0;
+    margin: 0 2px;
+    padding: 0.35em 0.625em 0.75em;
+  }
+
+  legend {
+    border: 0;
+    padding: 0;
+  }
+
+  textarea {
+    overflow: auto;
+    resize: vertical;
+  }
+
+  optgroup {
+    font-weight: bold;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  td, th {
+    padding: 0;
+  }
+
+  html, button, input, select, textarea {
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, p, figure, form, blockquote {
+    margin: 0;
+  }
+
+  ul, ol, li, dl, dd {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul, ol {
+    list-style: none outside none;
+  }
+
+  h1, h2, h3 {
+    line-height: 2;
+    font-weight: normal;
+  }
+
+  h1 {
+    font-size: 1.8rem;
+  }
+
+  h2 {
+    font-size: 1.6rem;
+  }
+
+  h3 {
+    font-size: 1.4rem;
+  }
+
+  input::-moz-placeholder, textarea::-moz-placeholder {
+    color: #cccccc;
+  }
+
+  input:-ms-input-placeholder, textarea:-ms-input-placeholder {
+    color: #cccccc;
+  }
+
+  input::-webkit-input-placeholder, textarea::-webkit-input-placeholder {
+    color: #cccccc;
+  }
+
+  * {
+    -webkit-overflow-scrolling: touch;
+    box-sizing: border-box;
+  }
+
+  ::-webkit-scrollbar { /*滚动条整体样式*/
+    width: 10px; /*高宽分别对应横竖滚动条的尺寸*/
+    height: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb { /*滚动条里面小方块*/
+    border-radius: 5px;
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 1);
+    background: rgba(0, 0, 0, 0);
+  }
+
+  ::-webkit-scrollbar-track { /*滚动条里面轨道*/
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    background: rgba(0, 0, 0, 0);
+  }
+
+  .ell {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .fl {
+    float: left;
+  }
+
+  .fr {
+    float: right;
+  }
+
+  .clearFix:after {
+    content: ".";
+    display: block;
+    clear: both;
+    visibility: hidden;
+    line-height: 0;
+    height: 0;
+    font-size: 0;
+  }
+
+  .clearFix {
+    *zoom: 1;
+  }
+
+  [v-cloak] {
+    display: none;
+  }
+
+  .tac {
+    text-align: center;
+  }
+
+  .fwb {
+    font-weight: bold;
+  }
+
+  .t12 {
+    font-size: 1.2rem;
+  }
+
+</style>
---
+++ pages/one.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    one
+    <button @click="dd">111</button>
+    <nuxt-link to="/">index</nuxt-link>
+  </div>
+</template>
+
+<script>
+  import {getData} from '@/plugins/axios.js'
+
+  export default {
+    methods: {
+      dd() {
+        getData('1', 'put', {}).then(res => {
+          console.log(res)
+        }).catch(err => {
+          console.log(err)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
